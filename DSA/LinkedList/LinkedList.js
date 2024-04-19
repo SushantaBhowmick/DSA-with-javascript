79673a0efdd414468445c04fd8a8e1ec38bcd331
Sushanta Bhowmick <bhosushanta922@gmail.com>
2024-04-20 01:10:41 +0530

    add more linkedlist methods

--- DSA/LinkedList/LinkedList.js
+++ DSA/LinkedList/LinkedList.js
@@ -23,6 +23,11 @@ class LinkedList{
         this.size++
     }
 
+    deleteFrom1st(){
+        this.head = this.head.next;
+        this.size--
+    }
+
     // append(value){
     //     const node = new Node(value)
     //     if(this.isEmpty()){
@@ -54,6 +59,62 @@ class LinkedList{
         this.size++
     }
 
+    deleteFromEnd(){
+        if(this.isEmpty()){
+            return console.log('Linked list has empty')
+        }else{
+            let curr = this.head;
+            let prev;
+            while(curr.next){
+                prev = curr;
+                curr=curr.next
+            }
+            prev.next = null
+        }
+        this.size--
+
+    }
+
+    insertAt(value,index){
+        if(index<0 || index===undefined){
+            return console.log('index must be a positive value')
+        }
+        if(index===0){
+            this.prepend(value)
+        }else{
+            const node = new Node(value)
+            let count =0;
+            let curr = this.head;
+            let prev;
+            while(count<index){
+                prev = curr;
+                curr = curr.next;
+                count++
+            }
+            prev.next = node;
+            node.next = curr
+        }
+        this.size++;
+    }
+    deleteAt(index){
+        if(index<0 || index===undefined){
+            return console.log('index must be a positive value')
+        }
+        if(index===0){
+            this.deleteFrom1st
+        }else{
+            let count = 0;
+            let curr = this.head;
+            let prev;
+            while(count<index){
+                 prev = curr;
+                 curr = curr.next  
+                 count++
+            }
+            prev.next=curr.next;
+        }
+    }
+
     print(){
         if(this.isEmpty()){
             return console.log("LinkedList is empty")
@@ -83,8 +144,13 @@ list.prepend(10)
 list.prepend(5)
 list.prepend(20)
 list.append(50)
+// list.deleteFrom1st()
 list.append(60)
 list.append(40)
+list.print()
+list.insertAt(2,5)
+// list.deleteFromEnd()
+list.deleteAt(4)
 list.print()
 // list.print()
 // console.log(list.isEmpty())

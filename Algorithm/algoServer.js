0a55741123ede9c0ab5b5e754ab9ba56767e981a
Sushanta Bhowmick <bhosushanta922@gmail.com>
2025-02-21 01:00:11 +0530

    prime,fact

---
+++ Algorithm/algoServer.js
@@ -0,0 +1,2 @@
+// const fact2 = require('./factorial/fact2')
+const prime2= require('./prime/prime2')
--- server.js
+++ server.js
@@ -2,24 +2,8 @@ const express = require('express')
 const cluster = require('cluster')
 const os = require('os')
 
-// const prime = require('./prime/prime')
-// const fact = require('./factorial/fact')
-// const prime = require('./fibonacci/recursiveFibo')
-// const prime = require('./factorial/recursiveFact')
-// const js = require('./JS/scope')
-// const obj = require('./JS/object')
-// const arr = require('./JS/arrays')
-
-// const arrMethods = require('./JS/array/array')
 // const dsaArr = require('./DSA/Array/arrayManage')
-
-// const lcs = require('./DSA/String/string')
-// const LinkedList = require('./DSAv /LinkedList/LinkedList')
-
-// const pow = require('./pow')
-// const gcd = require('./gcd')
-const fibonacci2  = require('./Algorithm/fibonacci/fibonacci2')
-
+const Algorithm = require('./Algorithm/algoServer')
 
 const app = express()
 const Port = 4000;
